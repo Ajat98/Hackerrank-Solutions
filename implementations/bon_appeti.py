becbe9cce444af53df1798c1a438ff12c3cb3c9f
Adam Harner <adamj.harner@gmail.com>
2020-03-03 19:41:15 -0500

    created bon_appeti.py
    https://www.hackerrank.com/challenges/bon-appetit/problem?isFullScreen=true&h_r=next-challenge&h_v=zen

---
+++ implementations/bon_appeti.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+
+    total = 0
+    for i in bill:
+        total += i
+    
+    annas = (total - bill[k]) * 0.5
+
+    if annas != b:
+        print(int(b - annas))
+    else: 
+        print("Bon Appetit")
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
