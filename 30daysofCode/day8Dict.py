d94f3d7c0dc8e133fbc0fc8ca45acd1fd2fe040c
Adam Harner <adamj.harner@gmail.com>
2020-04-03 11:27:14 -0400

    updated day8.py

--- 30daysofCode/day8Dict.py
+++ 30daysofCode/day8Dict.py
@@ -1,23 +1,24 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-
 d = {}
 n = int(input())
 
 for i in range(n):
-    s = input() 
+    s = str(input()) 
 
-    a = s.split(" ")
+    a = s.split()
     
     
     if a[0] not in d:
         d[a[0]] = int(a[1])
 
 
-for i in range(n): 
-    k = input()
-    if k in d.keys():
-        print(str(k) + "=" + str(d[k]))
-    else:
-        print("Not found")
-
+try:
+    while True: 
+        k = input()
+        if k in d:
+            print(str(k) + "=" + str(d[k]))
+        else:
+            print("Not found")
+except EOFError:
+    pass
